fc927d0c230d1c45f72130eacf96836ba8d47b09
ethankelly <72452934+ethankelly@users.noreply.github.com>
2021-03-19 14:03:53 +0000

    Restructured project

--- src/graph.py
+++ src/graph.py
@@ -1,20 +1,29 @@
+import glob
+import os.path
+
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
-import os.path
 
 
 def show_graph(filepath):
+    # Convert CSV into numpy matrix
     data = np.genfromtxt(filepath, delimiter=',', dtype=int)
     graph = nx.from_numpy_matrix(data)
+
+    # Ensure layouts are displayed using a force-directed (Kamada-Kawai) layout
     pos = nx.kamada_kawai_layout(graph, scale=1, dim=2)
     nx.draw(graph, pos, node_color='#007a5a', node_size=300, font_size=10, with_labels=True)
+
+    # Save to a .jpg file in the output file
     filename = os.path.splitext(filepath)[0]
     plt.savefig(f"{filename}.jpg", format="JPEG")
     plt.show()
 
 
-files = ["tree.csv", "4regular.csv", "erdosRenyi.csv", "complete.csv"]
+files = []
+for file in glob.iglob("graphs/*.csv", recursive=True):
+    files.append(file)
 
 for file in files:
     show_graph(file)
