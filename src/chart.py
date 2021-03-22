e8225c47184927caa1d78b3f3976227b20714f3f
ethankelly <72452934+ethankelly@users.noreply.github.com>
2021-03-22 13:27:19 +0000

    Refactoring

--- src/chart.py
+++ src/chart.py
@@ -2,19 +2,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Read in data using pandas and print to verify
 data = pd.read_csv("charts/MixedData.csv")
-
-outbreak = data['OUTBREAK']
-infections = data['INFECTED']
-strategy = data['STRATEGY']
-
 print(data)
 
+# Scatter plot using seaborn to colour data points by strategy
+sns.scatterplot(x=data['OUTBREAK'], y=data['INFECTED'], hue=data['STRATEGY'])
 
-sns.scatterplot(outbreak, infections, hue=strategy)
-plt.title('Total infections by outbreak for each defence strategy')
-plt.xlabel('Outbreak')
-plt.ylabel('Infections')
-# df.plot(kind='density')  # estimate density function
-# df.plot(kind='hist')  # histogram
+# Setting the chart title and axis labels
+font = {'fontname': 'Helvetica Neue'}
+plt.title('Total infections by outbreak for each defence strategy', **font)
+plt.xlabel('Outbreak', **font)
+plt.ylabel('Infections', **font)
 plt.show()
