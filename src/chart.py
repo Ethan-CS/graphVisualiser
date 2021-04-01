4e59d683ecc48fc37d5266db5a09ae432ec1e8d7
ethankelly <72452934+ethankelly@users.noreply.github.com>
2021-04-01 22:45:22 +0100

    Percentage infected plots generated

--- src/chart.py
+++ src/chart.py
@@ -1,24 +1,34 @@
+import glob
 import os
+import re
 
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
+path = "/Users/ethankelly/Documents/Agency/data"
+strategies = ["Deterministic", "Mixed", "Random"]
+graph_types = ["Erdős–Rényi"]
 
-def create_graph(data, strategy_name):
+
+def create_win_plot(data_file, strategy_name):
     """
     Reads in total number of wins of models from a data csv file
     and creates a scatter plot of the strategy results for the
     given protection allocation strategy.
     """
-    data = pd.read_csv(data)
-    print(data)  # Print the data to verify
+    data = pd.read_csv(data_file)
 
     # Scatter plot using seaborn to colour data points by strategy
+    plt.figure(figsize=(10, 8))
     sns.scatterplot(x=data['P VALUE'], y=data['NUMBER OF WINS'], hue=data['DEFENCE STRATEGY'], palette="bright")
 
     # Setting the chart title and axis labels
-    font = {'fontname': 'Helvetica Neue'}
+    font = {'family': 'serif',
+            'color': 'black',
+            'weight': 'normal',
+            'size': 16,
+            }
     plt.title(f'Winners for each defence strategy with {strategy_name} protection allocation', **font)
     plt.xlabel('Graph density', **font)
     plt.ylabel('Number of wins', **font)
@@ -26,30 +36,81 @@ def create_graph(data, strategy_name):
     plt.show()
 
 
-def get_win_charts(path, strategies, graph_types):
+def get_win_charts(path_to_win_files, all_strategies, all_graph_types):
     """
     Given a root filepath, a list of strategies and a list of graph types,
     Creates a graph for each of the winning strategy files and returns the file paths.
 
-    :param path: the root directory in which the winning data files can be found.
-    :param strategies: a list of all strategies used in the models.
-    :param graph_types: a list of all graph types that we have results to output for.
+    :param path_to_win_files: the root directory in which the winning data files can be found.
+    :param all_strategies: a list of all strategies used in the models.
+    :param all_graph_types: a list of all graph types that we have results to output for.
     """
     win_results = []
-    for graph in graph_types:
-        for strategy in strategies:
-            win_result = f"{path}/{graph}/{strategy}Winner.csv"
+    for graph in all_graph_types:
+        for strategy in all_strategies:
+            win_result = f"{path_to_win_files}/{graph}/{strategy}Winner.csv"
             if os.path.exists(win_result):
                 win_results.append(win_result)
-                create_graph(win_result, strategy)
+                create_win_plot(win_result, strategy)
+
+
+def create_percent_infected_plot(data, allocation, num_vertices):
+    # Scatter plot using seaborn to colour data points by strategy
+    plt.figure(figsize=(10, 8))
+    sns.scatterplot(
+        x=data['P VALUE'], y=(data['INFECTED'] / num_vertices), hue=data['STRATEGY'], palette="bright", s=60)
+    leg = plt.legend(title="Defence Strategies")
+    for i in range(len(strategies)):
+        leg.get_texts()[i].set_text(strategies[i])
+
+    font = {'family': 'serif',
+            'color': 'black',
+            'weight': 'normal',
+            'size': 16,
+            }
+    plt.title(f'Percentage infections by defence \n({allocation} protection allocation)', **font)
+    plt.xlabel('Graph density', **font)
+    plt.ylabel('Percent of graph infected', **font)
+    plt.savefig(f"charts/{allocation}.jpg", format="JPEG")
+    plt.show()
+
+
+def combine_data(this_path, strategy, p):
+    strategy_directory = f"{this_path}/{strategy}/"
+    data_files = glob.glob(strategy_directory + f"/{strategy}Data*.csv")
+    all_dataframes = []
+    for data_file in data_files:
+        data = pd.read_csv(data_file)
+        data.insert(0, "P VALUE", p)
+        all_dataframes.append(data)
+    return pd.concat(all_dataframes)
+
+
+def atof(text):
+    try:
+        ret_val = float(text)
+    except ValueError:
+        ret_val = text
+    return ret_val
+
+
+def natural_keys(text):
+    return [atof(c) for c in re.split(r'[+-]?([0-9]+(?:[.][0-9]*)?|[.][0-9]+)', text)]
 
 
 def main():
-    path = "/Users/ethankelly/Documents/Agency/data"
-    strategies = ["Deterministic", "Mixed", "Random"]
-    graph_types = ["Erdős–Rényi"]
-
     get_win_charts(path, strategies, graph_types)
+
+    # all_paths is a list of all directories in the specified path directory
+    all_paths = [f.path for f in os.scandir(path + "/" + graph_types[0]) if f.is_dir()]
+    all_paths.sort(key=natural_keys)
+    ds_by_strategy = [[], [], []]
+    for each_path in all_paths:
+        for i in range(len(strategies)):
+            ds_by_strategy[i].append(combine_data(each_path, strategies[i], p=each_path.split('/')[-1]))
+
+    for strategy in strategies:
+        create_percent_infected_plot(pd.concat(ds_by_strategy[strategies.index(strategy)]), strategy, num_vertices=50)
 
 
 if __name__ == "__main__":
