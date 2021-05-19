2f0ca1b2355eda3fc6c34fa349656f94a54a8830
ethankelly <72452934+ethankelly@users.noreply.github.com>
2021-05-19 09:54:40 +0100

    Refactoring and Testing

--- src/chart.py
+++ src/chart.py
@@ -1,25 +1,59 @@
 import glob
 import os
 import re
+import csv
 
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-data_filepath = "/Users/ethankelly/IdeaProjects/Agency/data"
+data_filepath = "/Users/ethankelly/Documents/Agency/data"
+graph_filepath = f'{data_filepath}/Erdos Renyi 0.05-0.95/0.05/Graph0.csv'
 out = "charts"
 protection_allocation = ["Deterministic", "Mixed", "Random"]
 defence_strategies = ["Proximity", "Degree", "Protection"]
-graph_types = ["Preferential Attachment"]
-ranges = ["1 - 5"]
-font = {'family': 'serif',
-        'color': 'black',
-        'weight': 'normal',
-        'size': 20,
-        }
-num_v = 30
-data_filter = 'MIN DEGREE'
-x_lab = data_filter.title()
+graph_types = ["Erdos Renyi"]
+ranges = ["0.05-0.95"]
+font = {'family': 'serif', 'color': 'black', 'weight': 'normal', 'size': 20}
+num_v = len(next(csv.reader(open(graph_filepath, 'r'))))
+filter_by = 'P VALUE'
+x_lab = filter_by.title()
+
+
+def get_degree_distributions(graphs, data_path):
+    plt.figure(figsize=(14, 10))
+    sns.set(rc={"figure.dpi": 300, 'savefig.dpi': 300})
+    sns.set_context('notebook')
+    sns.set_style("ticks")
+    plt.xlabel(filter_by.title(), **font)
+    my_dict = dict()
+    for g in graphs:
+        for r in ranges:
+            paths = glob.glob(f"{data_path}/{g} {r}/*/")
+            paths.sort(key=natural_keys)
+            probabilities = list()
+            for path in paths:
+                prob = float(path.split('/')[-2])
+                probabilities.append(prob)
+                my_dict[prob] = list()
+                with open(f"{path}Degrees.csv", 'r') as csv_file:
+                    import csv
+                    degrees = csv.reader(csv_file, delimiter=',')
+                    for row in degrees:
+                        for entry in row:
+                            my_dict[prob].append(int(entry))
+            data = [*zip(*my_dict.items())][1]  # 'transpose' items to parallel key, value lists
+            sns.boxplot(data=data, hue_order=probabilities)
+            plt.xticks(range(len(probabilities)), probabilities)
+
+            plt.title("Degree distributions", **font)
+            plt.ylabel("Degree distribution", **font)
+            where_to_save = f"{out}/degrees/{r}"
+            if not os.path.exists(where_to_save):
+                os.makedirs(where_to_save)
+
+            plt.savefig(where_to_save + "/Degrees.jpg", format="JPEG")
+            plt.show()
 
 
 def get_win_charts(graphs, data_path):
@@ -34,10 +68,10 @@ def get_win_charts(graphs, data_path):
                 if os.path.exists(win_result):
                     data = pd.read_csv(win_result)
                     create_plot(type_of_plot='win', data=data, allocation=allocation, value_range=each_range,
-                                filter_by=data_filter)
+                                filter_by_key=filter_by)
 
 
-def create_plot(type_of_plot, data, allocation, value_range, filter_by):
+def create_plot(type_of_plot, data, allocation, value_range, filter_by_key):
     """
     Reads in total number of wins of models from a data csv file
     and creates a scatter plot of the strategy results for the
@@ -49,28 +83,31 @@ def create_plot(type_of_plot, data, allocation, value_range, filter_by):
     sns.set_context('notebook')
     sns.set_style("ticks")
     where_to_save = f"{out}/"
-    plt.xlabel(data_filter.title(), **font)
+    plt.xlabel(filter_by_key.title(), **font)
+
     if type_of_plot == 'win':
-        sns.lineplot(x=data[filter_by], y=data['NUMBER OF WINS'], hue=data['DEFENCE STRATEGY'],
+        where_to_save += f"winners/{value_range}"
+        sns.lineplot(x=data[filter_by_key], y=data['NUMBER OF WINS'], hue=data['DEFENCE STRATEGY'],
                      palette="bright", linewidth=5, alpha=0.8)
         plt.title(f'Winners for each defence strategy\n({allocation} protection allocation)', **font)
-        plt.xlabel(data_filter.title(), **font)
+        plt.xlabel(filter_by_key.title(), **font)
         plt.ylabel('Number of wins', **font)
         leg = plt.legend(title="Defence Strategies")
         for i in range(len(defence_strategies)):
             leg.get_texts()[i].set_text(defence_strategies[i])
-        where_to_save += f"winners/{value_range}/{allocation}.jpg"
-    elif type_of_plot == 'percent infected':
-        sns.violinplot(x=data[filter_by], y=(data['INFECTED'] / num_v), hue=data['STRATEGY'], palette="bright")
+    elif type_of_plot == 'percent':
+        where_to_save += f"percent_infected/{value_range}"
+        sns.boxplot(x=data[filter_by_key], y=(data['INFECTED'] / num_v), hue=data['STRATEGY'], palette="bright")
         plt.title(f'Percentage infections by defence\n({allocation} protection allocation)', **font)
-        plt.xlabel(data_filter.title(), **font)
+        plt.xlabel(filter_by_key.title(), **font)
         plt.ylabel('Percent of graph infected', **font)
         leg = plt.legend(title="Defence Strategies")
         for i in range(len(defence_strategies)):
             leg.get_texts()[i].set_text(defence_strategies[i])
-        where_to_save += f"percent_infected/{value_range}/{allocation}.jpg"
 
-    plt.savefig(where_to_save, format="JPEG")
+    if not os.path.exists(where_to_save):
+        os.makedirs(where_to_save)
+    plt.savefig(where_to_save + f"/{allocation}.jpg", format="JPEG")
     plt.show()
 
 
@@ -85,12 +122,12 @@ def get_infected_plots(data_path):
                 for i in range(len(protection_allocation)):
                     ds_by_strategy[i].append(
                         combine_data(this_path=path, strategy=protection_allocation[i], extra_val=path.split('/')[-1],
-                                     data_filter=data_filter))
+                                     data_filter=filter_by))
 
             for allocation in protection_allocation:
                 data = pd.concat(ds_by_strategy[protection_allocation.index(allocation)])
-                create_plot(type_of_plot='percent infected', data=data, allocation=allocation,
-                            value_range=p_range, filter_by=data_filter)
+                create_plot(type_of_plot='percent', data=data, allocation=allocation,
+                            value_range=p_range, filter_by_key=filter_by)
 
 
 def combine_data(this_path, strategy, extra_val, data_filter):
@@ -119,6 +156,7 @@ def natural_keys(text):
 
 
 def main():
+    get_degree_distributions(graph_types, data_filepath)
     get_win_charts(graph_types, data_filepath)
     get_infected_plots(data_filepath)
 
--- src/graph.py
+++ src/graph.py
@@ -1,13 +1,16 @@
 import glob
+import json
 import os.path
 
 import matplotlib.pyplot as plt
+import networkx
 import networkx as nx
 import numpy as np
 
 
-def show_graph(filepath):
-    plt.figure(figsize=(10, 8))
+def draw_graph_from_file(filepath):
+    plt.figure(figsize=(14, 10))
+    plt.rcParams['figure.dpi'] = 300
     # Convert CSV into numpy matrix
     data = np.genfromtxt(filepath, delimiter=',', dtype=int)
     graph = nx.from_numpy_matrix(data)
@@ -22,6 +25,18 @@ def show_graph(filepath):
     plt.show()
 
 
+def draw_graph(graph, name, filepath):
+    plt.figure(figsize=(14, 10))
+    plt.rcParams['figure.dpi'] = 300
+    # Ensure layouts are displayed using a force-directed (Kamada-Kawai) layout
+    pos = nx.spring_layout(graph, scale=1, dim=2)
+    nx.draw(graph, pos, width=6, edge_color='#1f4d3c', node_color='#2bd999', node_size=2000, font_size=25,
+            font_weight='bold', with_labels=False)
+    # Save to a .jpg file in the output file
+    plt.savefig(f"{filepath}/{name}.jpg", format="JPEG", dpi=300)
+    plt.show()
+
+
 def get_files(source_dir):
     # Add the name of each graph CSV file to the list of files and return the list
     files = []
@@ -31,5 +46,6 @@ def get_files(source_dir):
     return files
 
 
-for file in get_files('graphs'):
-    show_graph(file)
+# for file in get_files('graphs'):
+#    draw_graph_from_file(file)
+draw_graph(nx.random_tree(5), 'tree', 'graphs')
